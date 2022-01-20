25aee574e98531f34470588031f6f8f2252be809
Janainaf <figueira.janaina@gmail.com>
2022-01-20 18:47:19 +0100

    added dynamic route, needs refactoring

--- app.js
+++ app.js
@@ -1,21 +1,30 @@
 const express = require("express");
 const path = require("path");
 const data = require("./data.json");
-const port = 30010;
+const port = 3000;
 const app = express();
 
 app.set("view engine", "pug");
 
 app.use("/static", express.static(path.join(__dirname, "public")));
 
-app.get("/about", function (req, res) {
-  res.render("about");
-});
-
 app.get("/", function (req, res) {
   res.locals = data.projects;
   res.render("index", { data: data.projects });
 });
+
+app.get("/about", function (req, res) {
+  res.render("about");
+});
+
+app.get("/projects/:id", function (req, res) {
+  res.locals = data;
+  const id = req.params.id;
+  res.render("project", { project: data.projects[id] });
+  // have to check for length - error if not found
+  // and /0 is awful
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
